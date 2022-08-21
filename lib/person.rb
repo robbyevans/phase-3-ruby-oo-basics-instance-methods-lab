81a0f5dce8681926f8cee8052cb546df14c98713
Evans Rutto <robbyevans001@gmail.com>
2022-08-21 17:04:05 +0300

    done

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+
+class Person
+  def talk
+    puts "Hello World!"
+  end     
+  def walk
+    puts "The Person is walking"
+  end
+end
+
+robby=Person.new
+robby.talk
+robby.walk
+
+jobez=Person.new
+jobez.talk
+jobez.walk
